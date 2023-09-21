654ce33d2a2814823c83d945550f69a567a2ead7
trashpirate <nadinaoates@gmail.com>
2023-09-21 16:03:49 -0500

    minor updates

--- frontend/components/nftInfo/index.tsx
+++ frontend/components/nftInfo/index.tsx
@@ -53,7 +53,7 @@ export default function NFTInfo() {
         <div>{`Total NFTs minted: ${totalSupply}`}</div>
         <div>{`Remaining NFTs to mint: ${remainingSupply}`}</div>
       </div>
-      <a href="https://opensea.io/collection/plots-4" className={styles.button}>
+      <a href="https://opensea.io/collection/tgplots" className={styles.button}>
         View on OpenSea
       </a>
     </div>
